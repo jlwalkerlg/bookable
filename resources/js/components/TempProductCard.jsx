1ef4b05375c31365ec555199559a3104d0575b9e
Jordan Walker <walker.jlg@gmail.com>
2019-06-14 14:19:29 +0100

    Update TempProductCard

--- resources/js/components/TempProductCard.jsx
+++ resources/js/components/TempProductCard.jsx
@@ -5,7 +5,7 @@ const TempProductCard = ({ title, author, image, price, ...props }) => {
   const book = {
     id: 1,
     title,
-    author,
+    author: { id: 1, name: author },
     author_id: 1,
     image_url: image,
     price
