82f92db61743a9f6a5a6ef25b2d49fd7043ce39a
Jordan Walker <walker.jlg@gmail.com>
2019-06-16 10:38:57 +0100

    Hide quantity input if book in cart

--- resources/js/views/books/Show.jsx
+++ resources/js/views/books/Show.jsx
@@ -171,30 +171,33 @@ class Show extends Component {
                 <Form
                   action="/"
                   method="POST"
+                  className="mb-3"
                   onSubmit={
                     inCart
                       ? e => this.removeFromCart(e, book)
                       : e => this.addToCart(e, book)
                   }
                 >
-                  <div className="d-inline-block mr-3">
-                    {/* Quantity */}
-                    <Form.Group controlId="quantity" className="mb-3">
-                      <Form.Label className="text-description mb-0 d-inline-block mb-2">
-                        Quantity:
-                      </Form.Label>
-                      <Form.Control
-                        type="number"
-                        className="rounded-pill bg-transparent w-auto"
-                        value={quantity}
-                        min="1"
-                        onChange={this.changeQuantity}
-                        size="sm"
-                      />
-                    </Form.Group>
-                  </div>
+                  {/* Quantity */}
+                  {!inCart && (
+                    <div className="d-inline-block mr-3">
+                      <Form.Group controlId="quantity">
+                        <Form.Label className="text-description mb-0 d-inline-block mb-2">
+                          Quantity:
+                        </Form.Label>
+                        <Form.Control
+                          type="number"
+                          className="rounded-pill bg-transparent w-auto"
+                          value={quantity}
+                          min="1"
+                          onChange={this.changeQuantity}
+                          size="sm"
+                        />
+                      </Form.Group>
+                    </div>
+                  )}
+                  {/* Submit button */}
                   <div className="d-inline-block">
-                    {/* Button */}
                     <Button
                       variant="warning"
                       type="submit"
