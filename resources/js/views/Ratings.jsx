1a83165db70a63e117d107b16f482284dc253831
Jordan Walker <walker.jlg@gmail.com>
2019-06-26 16:37:50 +0100

    Ratings loading and error handling

--- resources/js/views/Ratings.jsx
+++ resources/js/views/Ratings.jsx
@@ -4,14 +4,23 @@ import axios from 'axios';
 import { Container } from 'react-bootstrap';
 import Pagination from '../components/Pagination';
 import URL from '../utils/URL';
-import Loading from '../components/Loading';
 import BookListing from '../components/BookListing';
 import RatingsTable from '../components/RatingsTable';
 import { addRating, updateRating, deleteRating } from '../actions/ratings';
+import Async from '../components/Async';
 
 class Ratings extends Component {
   state = {
-    loading: true,
+    loading: {
+      user: true,
+      ratings: true,
+      userRatings: true
+    },
+    errors: {
+      user: null,
+      ratings: null,
+      userRatings: null
+    },
     user: null,
     ratings: null,
     userRatings: null,
@@ -19,37 +28,18 @@ class Ratings extends Component {
     limit: 10
   };
 
-  async componentDidMount() {
-    await this.fetchData();
-    this.setState({ loading: false });
+  componentDidMount() {
+    this.fetchRatings();
   }
 
-  async componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps) {
     if (prevProps.location.search !== this.props.location.search) {
-      this.setState({ loading: true });
-      await this.fetchData();
-      this.setState({ loading: false });
+      this.fetchRatings();
     }
   }
 
-  async fetchData() {
-    const { user, ratings, count } = await this.fetchRatings();
-
-    if (!this.props.user.id) {
-      return this.setState({ user, ratings, count });
-    }
-
-    if (user.id === this.props.user.id) {
-      return this.setState({ user, ratings, userRatings: ratings, count });
-    }
-
-    const bookIds = ratings.map(rating => rating.book_id);
-    const userRatings = (await this.fetchUserRatings(bookIds)).ratings;
-
-    return this.setState({ user, ratings, userRatings, count });
-  }
-
-  async fetchRatings() {
+  fetchRatings = async () => {
+    this.setLoading({ user: true, ratings: true });
     const { limit } = this.state;
     const { userId } = this.props.match.params;
     const offset = this.calcOffset();
@@ -57,22 +47,62 @@ class Ratings extends Component {
       const response = await axios.get(`/api/users/${userId}/ratings`, {
         params: { limit, offset, count: true, with: 'book.author' }
       });
-      return response.data;
+      const { user, ratings, count } = response.data;
+      this.setState({ user, ratings, count });
+      this.setError({
+        user: null,
+        ratings: null
+      });
+
+      this.fetchUserRatings(ratings);
     } catch (error) {
-      console.log(error);
+      this.setError({
+        user: error.response.statusText,
+        ratings: error.response.statusText
+      });
     }
+    this.setLoading({ user: false, ratings: false });
+  };
+
+  fetchUserRatings = async ratings => {
+    this.setLoading({ userRatings: true });
+
+    const userId = parseInt(this.props.match.params.userId);
+    const authUser = this.props.user;
+    if (!authUser.id) {
+      this.setError({ userRatings: null });
+      this.setLoading({ userRatings: false });
+      return;
+    }
+    if (userId === authUser.id) {
+      this.setState({ userRatings: ratings });
+      this.setError({ userRatings: null });
+      this.setLoading({ userRatings: false });
+      return;
+    }
+
+    const bookIds = (ratings || this.state.ratings).map(
+      rating => rating.book_id
+    );
+    try {
+      const response = await axios.get(`/api/ratingss`, {
+        params: { user_id: authUser.id, book_ids: bookIds.join(',') }
+      });
+      const userRatings = response.data.ratings;
+      this.setState({ userRatings });
+      this.setError({ userRatings: null });
+    } catch (error) {
+      this.setError({ userRatings: error.response.statusText });
+    }
+    this.setLoading({ userRatings: false });
+  };
+
+  setLoading(loading) {
+    this.setState({ loading: { ...this.state.loading, ...loading } });
   }
 
-  async fetchUserRatings(bookIds) {
-    const { user } = this.props;
-    try {
-      const response = await axios.get(`/api/ratings`, {
-        params: { user_id: user.id, book_ids: bookIds.join(',') }
-      });
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
+  setError(error) {
+    this.setState({ errors: { ...this.state.errors, ...error } });
   }
 
   calcOffset() {
@@ -139,62 +169,81 @@ class Ratings extends Component {
   }
 
   render() {
-    const { loading, user, ratings, count, limit } = this.state;
-
-    if (loading)
-      return (
-        <div className="vh-100-nav">
-          <Loading />
-        </div>
-      );
+    const { loading, errors, user, ratings, count, limit } = this.state;
 
     return (
       <div className="section">
         <Container>
-          <h1 className="h5 text-uppercase mb-0">{user.name}&apos;s Ratings</h1>
+          <Async
+            loading={loading.user}
+            error={errors.user}
+            retry={this.fetchRatings}
+          >
+            {() => (
+              <>
+                <h1 className="h5 text-uppercase mb-0">
+                  {user.name}&apos;s Ratings
+                </h1>
 
-          <RatingsTable
-            className="d-none d-md-table mt-3"
-            user={user}
-            ratings={ratings}
-            authUser={this.props.user}
-            userRatings={this.state.userRatings}
-            onAddRating={this.addRating}
-            onUpdateRating={this.handleUpdateRating}
-            onDeleteRating={this.deleteRating}
-          />
+                <Async
+                  loading={loading.ratings || loading.userRatings}
+                  error={errors.ratings || errors.userRatings}
+                  retry={
+                    errors.ratings
+                      ? this.fetchRatings
+                      : () => this.fetchUserRatings()
+                  }
+                >
+                  {() => (
+                    <>
+                      <RatingsTable
+                        className="d-none d-md-table mt-3"
+                        user={user}
+                        ratings={ratings}
+                        authUser={this.props.user}
+                        userRatings={this.state.userRatings}
+                        onAddRating={this.addRating}
+                        onUpdateRating={this.handleUpdateRating}
+                        onDeleteRating={this.deleteRating}
+                      />
 
-          <div className="d-md-none mt-3">
-            {ratings.map((rating, index) => {
-              const { book } = rating;
-              const userRating = this.getUserRating(rating);
+                      <div className="d-md-none mt-3">
+                        {ratings.map((rating, index) => {
+                          const { book } = rating;
+                          const userRating = this.getUserRating(rating);
 
-              return (
-                <BookListing
-                  key={index}
-                  user={user}
-                  authUser={this.props.user}
-                  book={book}
-                  author={book.author}
-                  rating={rating}
-                  userRating={userRating}
-                  createdAt={rating.created_at}
-                  onAddRating={this.addRating}
-                  onUpdateRating={this.handleUpdateRating}
-                  deletable={user.id === this.props.user.id}
-                  onDelete={e => this.deleteRating(e, userRating)}
-                />
-              );
-            })}
-          </div>
-          <Pagination
-            totalItems={count}
-            currentPage={this.getCurrentPage()}
-            pageSize={limit}
-            maxPages={5}
-            url={`${this.props.location.pathname}?page=`}
-            className="mt-3 justify-content-center pagination-warning"
-          />
+                          return (
+                            <BookListing
+                              key={index}
+                              user={user}
+                              authUser={this.props.user}
+                              book={book}
+                              author={book.author}
+                              rating={rating}
+                              userRating={userRating}
+                              createdAt={rating.created_at}
+                              onAddRating={this.addRating}
+                              onUpdateRating={this.handleUpdateRating}
+                              deletable={user.id === this.props.user.id}
+                              onDelete={e => this.deleteRating(e, userRating)}
+                            />
+                          );
+                        })}
+                      </div>
+                      <Pagination
+                        totalItems={count}
+                        currentPage={this.getCurrentPage()}
+                        pageSize={limit}
+                        maxPages={5}
+                        url={`${this.props.location.pathname}?page=`}
+                        className="mt-3 justify-content-center pagination-warning"
+                      />
+                    </>
+                  )}
+                </Async>
+              </>
+            )}
+          </Async>
         </Container>
       </div>
     );
