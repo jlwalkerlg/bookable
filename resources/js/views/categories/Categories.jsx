efa57d0b1cbbb90ed610598b7fb371f7d1b97dd0
Jordan Walker <walker.jlg@gmail.com>
2019-07-05 10:06:39 +0100

    Refactor categories

--- resources/js/views/categories/Categories.jsx
+++ resources/js/views/categories/Categories.jsx
@@ -2,21 +2,16 @@ import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Async from '../../components/Async';
+import Loading from '../../components/Loading';
 
 class Categories extends Component {
   state = {
-    loading: true,
+    isLoading: true,
     error: null,
-    categories: null
+    categories: []
   };
 
   async componentDidMount() {
-    this.fetchCategories();
-  }
-
-  fetchCategories = async () => {
-    this.setState({ loading: true });
     try {
       const response = await axios.get('/api/categories');
       const categories = response.data.categories.sort((a, b) => {
@@ -24,32 +19,32 @@ class Categories extends Component {
         if (a.name < b.name) return -1;
         return 0;
       });
-      this.setState({ categories, loading: false, error: null });
+      this.setState({ categories, isLoading: false });
     } catch (error) {
-      this.setState({ loading: false, error: error.response.statusText });
+      this.setState({ error, isLoading: false });
     }
-  };
+  }
 
   render() {
-    const { categories, loading, error } = this.state;
+    const { categories, isLoading, error } = this.state;
+
+    if (isLoading) return <Loading />;
+
+    if (error) return <p>Something went wrong: {error.message}.</p>;
 
     return (
       <main className="section bg-beige">
         <Container>
           <h1 className="h5 text-uppercase mb-4">Categories</h1>
-          <Async loading={loading} error={error} retry={this.fetchCategories}>
-            {() => (
-              <ul className="list-unstyled categories-list">
-                {categories.map((category, index) => (
-                  <li key={index} className="categories-list__category">
-                    <Link to={`/category/${category.id}`}>
-                      <p>{category.name}</p>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </Async>
+          <ul className="list-unstyled categories-list">
+            {categories.map(category => (
+              <li key={category.id} className="categories-list__category">
+                <Link to={`/category/${category.id}`}>
+                  <p>{category.name}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </Container>
       </main>
     );
