11edc44cbde672aaab833f0a3185122feb5fd882
Jordan Walker <walker.jlg@gmail.com>
2019-07-10 12:20:43 +0100

    Update todo

--- resources/js/components/QuoteCard.jsx
+++ resources/js/components/QuoteCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, Form } from 'react-bootstrap';
+import { Card, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import sanitize from '../utils/sanitize';
 import SubmitButton from './SubmitButton';
