dc8267c500923e07c970d5ee459e7346fed6535c
Jordan Walker <walker.jlg@gmail.com>
2019-08-04 10:55:31 +0100

    Request assets via https only if app.env not local

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         href="https://fonts.googleapis.com/css?family=Material+Icons|Lato:300,400,700|Merriweather:300,700&display=swap"
         rel="stylesheet">
     <!-- CSS -->
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css', true) }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css', config('app.env') !== 'local') }}">
     {{-- Stripe --}}
     <script src="https://js.stripe.com/v3/"></script>
 </head>
@@ -19,7 +19,7 @@
 <body>
     <noscript>You need to enable JavaScript to run this app.</noscript>
     <div id="root"></div>
-    <script src="{{ asset('js/app.js', true) }}"></script>
+    <script src="{{ asset('js/app.js', config('app.env') !== 'local') }}"></script>
 </body>
 
 </html>
